e3152125dc920bb3a5a2897e7b645e887df909b5
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2015-10-05 12:03:07 +0530

    Testing Fading of Background

---
+++ Assets/scripts/backgroundFader.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var theBackground : GameObject;
+var timeToStay : int = 8; // 8 seconds
+var willFade = false;
+var currentTime : float;
+
+function Start () 
+{
+	currentTime=0.0f;
+}
+
+function Update () 
+{
+	if(Input.GetKey(KeyCode.K))
+	{
+		willFade=true;
+	}
+	if( willFade )
+	{
+		currentTime+=Time.deltaTime;
+		var a : Color;
+		//theBackground.SetActive(false);
+		a = theBackground.GetComponent(SpriteRenderer).color;
+		a.a =  1 - ( currentTime / timeToStay );
+		//a.a = Mathf.Lerp(1.0,0.0,8*Time.deltaTime);
+		if(a.a>=0)
+		{
+			theBackground.GetComponent(SpriteRenderer).color = a;
+		}
+		else
+		{
+			Debug.Log("Your End is now");
+		}
+	}
+}
